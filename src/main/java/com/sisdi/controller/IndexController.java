5d6a0d38c8c13f10eea1a511514c19a4d4d7b7b4
Kmontano06 <keniamontano06@gmail.com>
2020-10-05 22:50:53 -0600

    Cambios en para Login

--- src/main/java/com/sisdi/controller/IndexController.java
+++ src/main/java/com/sisdi/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.sisdi.controller;
 
+import com.sisdi.data.UserService;
 import com.sisdi.model.office;
 import com.sisdi.model.version;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,20 +19,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class IndexController {
-
     @GetMapping("/")
-    public String listOffice() {
-        return "redirect:index";
-    }
-
-    @GetMapping("/index")
-    public String showIndex(Model model) {
+    public String showIndex(Model model, @AuthenticationPrincipal User user, HttpSession session) {
+        UserService uServ=new UserService();
         OfficeController offC=new OfficeController();
-//        List<office> offices=offC.getListOffice();
+        List<office> offices=offC.getListOffice();
         log.info("ejecutando el controlador Oficios");
-//        model.addAttribute("offices", offices);
-        return "login";
+        log.info("usuario:"+user);
+        session.setAttribute("user", user);
+        session.setAttribute("usuarioCompleto", uServ.getUser(user.getUsername()));
+        model.addAttribute("user", user);
+        model.addAttribute("offices", offices);
+        return "index";
     }
+    
      @GetMapping("/list")
     public String listOff(Model model) {
         OfficeController offC=new OfficeController();
