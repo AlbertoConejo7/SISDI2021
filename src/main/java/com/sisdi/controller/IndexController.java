2909a4e592f2b2a717d07b7aa4b5aac3a527fd09
Kmontano06 <keniamontano06@gmail.com>
2020-10-17 16:45:49 -0600

    Conexion Login con la base de datos.

--- src/main/java/com/sisdi/controller/IndexController.java
+++ src/main/java/com/sisdi/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.sisdi.controller;
 
-import com.sisdi.data.UserService;
+import com.sisdi.data.UserData;
+import com.sisdi.model.Department;
+import com.sisdi.model.TempUser;
+import com.sisdi.model.UserEntity;
 import com.sisdi.model.office;
 import com.sisdi.model.version;
+import com.sisdi.service.DepartmentServiceImp;
+import com.sisdi.service.TempUserServiceImp;
+import com.sisdi.service.UserServiceImp;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,18 +28,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class IndexController {
+    @Autowired 
+    private TempUserServiceImp tempUserService;
+    
+    @Autowired 
+    private DepartmentServiceImp departmentService;
+    
+    @Autowired 
+    private UserServiceImp userService;
+    
+    @Autowired 
+    private UserData userData;
+     
     @GetMapping("/login")
     public String login(){
-        return "login";
+       return "login";
     }
     @GetMapping("/")
     public String showIndex(Model model, @AuthenticationPrincipal User user, HttpSession session) {
-        UserService uServ=new UserService();
         OfficeController offC=new OfficeController();
         List<office> offices=offC.getListOffice();
         log.info("ejecutando el controlador Oficios");
         session.setAttribute("user", user);
-        session.setAttribute("usuarioCompleto", uServ.getUser(user.getUsername()));
+        session.setAttribute("usuarioCompleto", userData.getUser(user.getUsername()));
         model.addAttribute("user", user);
         model.addAttribute("offices", offices);
         return "index";
