03b9fdf8da4608dbb7b31b2dd09e7813443b9312
Kmontano06 <keniamontano06@gmail.com>
2020-10-17 16:46:08 -0600

    Conexion Login con la base de datos.

--- src/main/java/com/sisdi/controller/OfficeController.java
+++ src/main/java/com/sisdi/controller/OfficeController.java
@@ -1,17 +1,23 @@
 package com.sisdi.controller;
 
+import com.sisdi.dao.TempUserDao;
+import com.sisdi.model.TempUser;
 import com.sisdi.model.office;
+import com.sisdi.service.TempUserServiceImp;
 //import com.sisdi.model.version;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,6 +32,7 @@ public class OfficeController {
         model.addAttribute("date", fecha);
         return "offices/addOffice";
     }
+    
     @GetMapping("/editOffice/{officeId}")
     public String editOffice(@PathVariable String officeId,Model model) {
         office officeAct=this.getOffice(officeId);
@@ -47,21 +54,20 @@ public class OfficeController {
     public String pendingOffice(Model model) {
         List<office> offices = this.getListOffice();
         log.info("ejecutando el controlador Oficios");
-        model.addAttribute("offices", offices);
+        model.addAttribute("officesPending", offices);
         return "offices/pendingOffice";
     }
    
-//    @PostMapping("/anularOficio")
-//    public String anularOficio(@RequestParam("officeActual") office offi, Model model){
-//        model.addAttribute("offi", offi);
-//        model.addAttribute("anulado", "Algun oficio fue anulado");
-//        return "offices/anular";
-//    }
+    @PostMapping("/anular")
+    public String anularOficio(Model model){
+        log.info("Oficio Anulado");
+        return "offices/listOffices";
+    }
     
     public List<office> getListOffice(){
          List<office> list=new ArrayList();         
          office o1= new office(1, "Esperando Respuesta", "Probar que funciona",fecha,"Oficio-MPSP-1-Prueba","Interno");
-         office o2= new office(2, "Respondido", "Probar que funciona 2",fecha,"Oficio-MPSP-2-Prueba","Externo");
+         office o2= new office(2, "Anulado", "Probar que funciona 2",fecha,"Oficio-MPSP-2-Prueba","Externo");
          office o3= new office(3, "Esperando Respuesta", "Probar que funciona 3",fecha,"Oficio-MPSP-3-Prueba","Externo");
          office o4= new office(4, "Esperando Respuesta", "Probar que funciona 4",fecha,"Oficio-MPSP-4-Prueba","Interno");
          office o5= new office(5, "Respondido", "Probar que funciona 5",fecha,"Oficio-MPSP-5-Prueba","Externo");
