57c3ca3cd8756b73b4910335b9bea18ae7a6d328
AZVal <AZVal@DESKTOP-2Q7CQ50>
2020-10-19 21:40:47 -0700

    Ventana de respuesta de oficio (link de responder cambiado)

--- src/main/java/com/sisdi/controller/OfficeController.java
+++ src/main/java/com/sisdi/controller/OfficeController.java
@@ -25,23 +25,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 @RequestMapping(value = "/offices")
 public class OfficeController {
-    private Date fecha=Date.valueOf(LocalDate.now());
-    
+
+    private Date fecha = Date.valueOf(LocalDate.now());
+
     @GetMapping("/addOffice")
     public String addOffice(Model model) {
         model.addAttribute("date", fecha);
         return "offices/addOffice";
     }
-    
+
+    @GetMapping("/responseOffice/{officeId}")
+    public String responseOffice(@PathVariable String officeId, Model model) {
+        office officeAct = this.getOffice(officeId);
+        model.addAttribute("date", fecha);
+        model.addAttribute("officeActual", officeAct);
+        model.addAttribute("title", "Ver Oficio");
+        return "offices/responseOffice";
+    }
+
     @GetMapping("/editOffice/{officeId}")
-    public String editOffice(@PathVariable String officeId,Model model) {
-        office officeAct=this.getOffice(officeId);
+    public String editOffice(@PathVariable String officeId, Model model) {
+        office officeAct = this.getOffice(officeId);
         model.addAttribute("date", fecha);
         model.addAttribute("officeActual", officeAct);
         model.addAttribute("title", "Ver Oficio");
         return "offices/editOffice";
     }
-    
+
     @GetMapping("/listOffices")
     public String listOffice(Model model) {
         List<office> offices = this.getListOffice();
@@ -49,7 +59,7 @@ public class OfficeController {
         model.addAttribute("offices", offices);
         return "offices/listOffices";
     }
-    
+
     @GetMapping("/pendingOffice")
     public String pendingOffice(Model model) {
         List<office> offices = this.getListOffice();
@@ -57,37 +67,38 @@ public class OfficeController {
         model.addAttribute("officesPending", offices);
         return "offices/pendingOffice";
     }
-   
+
     @PostMapping("/anular")
-    public String anularOficio(Model model){
+    public String anularOficio(Model model) {
         log.info("Oficio Anulado");
         return "offices/listOffices";
     }
-    
-    public List<office> getListOffice(){
-         List<office> list=new ArrayList();         
-         office o1= new office(1, "Esperando Respuesta", "Probar que funciona",fecha,"Oficio-MPSP-1-Prueba","Interno");
-         office o2= new office(2, "Anulado", "Probar que funciona 2",fecha,"Oficio-MPSP-2-Prueba","Externo");
-         office o3= new office(3, "Esperando Respuesta", "Probar que funciona 3",fecha,"Oficio-MPSP-3-Prueba","Externo");
-         office o4= new office(4, "Esperando Respuesta", "Probar que funciona 4",fecha,"Oficio-MPSP-4-Prueba","Interno");
-         office o5= new office(5, "Respondido", "Probar que funciona 5",fecha,"Oficio-MPSP-5-Prueba","Externo");
-        
-         list.add(o1);
-         list.add(o2);
-         list.add(o3);
-         list.add(o4);
-         list.add(o5);
-         
-         return list;
+
+    public List<office> getListOffice() {
+        List<office> list = new ArrayList();
+        office o1 = new office(1, "Esperando Respuesta", "Probar que funciona", fecha, "Oficio-MPSP-1-Prueba", "Interno");
+        office o2 = new office(2, "Anulado", "Probar que funciona 2", fecha, "Oficio-MPSP-2-Prueba", "Externo");
+        office o3 = new office(3, "Esperando Respuesta", "Probar que funciona 3", fecha, "Oficio-MPSP-3-Prueba", "Externo");
+        office o4 = new office(4, "Esperando Respuesta", "Probar que funciona 4", fecha, "Oficio-MPSP-4-Prueba", "Interno");
+        office o5 = new office(5, "Respondido", "Probar que funciona 5", fecha, "Oficio-MPSP-5-Prueba", "Externo");
+
+        list.add(o1);
+        list.add(o2);
+        list.add(o3);
+        list.add(o4);
+        list.add(o5);
+
+        return list;
     }
-     public office getOffice(String officeId){
-         List<office> list= this.getListOffice();
-         office aux =null;
-         for(office o:list){
-             if(o.getName().equals(officeId)){
-                 aux=o;
-             }
-         }
-         return aux;
+
+    public office getOffice(String officeId) {
+        List<office> list = this.getListOffice();
+        office aux = null;
+        for (office o : list) {
+            if (o.getName().equals(officeId)) {
+                aux = o;
+            }
+        }
+        return aux;
     }
 }
