b27fbf09bfed5c1e4049a3a0db5f03ef6e3d7d3b
Kmontano06 <keniamontano06@gmail.com>
2020-10-20 15:58:26 -0600

    Actualizacion de la class Office, y metodos para leer de BD

---
+++ src/main/java/com/sisdi/model/Office.java
@@ -0,0 +1,31 @@
+package com.sisdi.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="t_office")
+public class Office implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int INDX;
+    private String OFFNUMBER;
+    private String REASON;
+    private String NAME;
+    private Date INCORDATE;
+    private Date INCORTIME;
+    private Date DEADLINE;
+    private Date SESSIONDATE;
+    private String OBSERVATIONS;
+    private boolean PUBLIC;
+    private boolean NOTIFIED;
+    private int STATE;
+    private int TYPE_ID;
+    private int VERSION_ID;
+    private String USER_ID;
+    private String RECEIVER_ID;
+    private int TIMEOUTS_ID;
+}
