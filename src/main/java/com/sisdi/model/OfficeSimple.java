d5c43333ec49f2ffc8199bc6689f6ccc6789b371
Kmontano06 <keniamontano06@gmail.com>
2020-10-22 12:12:02 -0600

    Funciones para guardar Office

---
+++ src/main/java/com/sisdi/model/OfficeSimple.java
@@ -0,0 +1,18 @@
+package com.sisdi.model;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class OfficeSimple {
+    private String offnumber;
+    private String reason;
+    private String dateCreate;
+    private String emisor;
+    private String emisorDep;
+    private String receptor;
+    private String receptorDep;
+    private int type_id;
+    private String observations;
+    private String dateLimit;
+}
