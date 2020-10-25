4568275458da84d703f85505c45a5b3524052abb
Kmontano06 <keniamontano06@gmail.com>
2020-10-25 16:39:42 -0600

    Cambios para anular Oficio

--- src/main/java/com/sisdi/dao/OfficeDao.java
+++ src/main/java/com/sisdi/dao/OfficeDao.java
@@ -1,9 +1,16 @@
 package com.sisdi.dao;
 
 import com.sisdi.model.Office;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OfficeDao extends CrudRepository<Office, Integer>{
+//    @Modifying
+//    @Query("UPDATE T_OFFICE u SET u.STATE=:state WHERE u.OFFNUMBER=:offnumber")
+//    void updateState(@Param(value = "offnumber") String offnumber, @Param(value = "STATE") int state);
+
 }
