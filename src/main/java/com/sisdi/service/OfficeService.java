a7076b9882e2bc20111cdaaf0c7d024b34a73ba7
Kmontano06 <keniamontano06@gmail.com>
2020-10-25 20:36:09 -0600

    Union lista byEmisor y lista byReceptor

--- src/main/java/com/sisdi/service/OfficeService.java
+++ src/main/java/com/sisdi/service/OfficeService.java
@@ -3,7 +3,6 @@ package com.sisdi.service;
 import com.sisdi.model.Office;
 import java.util.List;
 
-//
 public interface OfficeService {
     List<Office> listarOficios();
     Office addOffice(Office office);
