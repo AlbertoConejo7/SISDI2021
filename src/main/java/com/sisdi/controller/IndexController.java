3ff7b1d7eca8fd431aa7a60a627c5beb0971a297
Kmontano06 <keniamontano06@gmail.com>
2021-03-30 19:50:34 -0600

    Notificacion de Oficios con fecha limite

--- src/main/java/com/sisdi/controller/IndexController.java
+++ src/main/java/com/sisdi/controller/IndexController.java
@@ -1,21 +1,22 @@
 package com.sisdi.controller;
 
 import com.sisdi.data.UserData;
-import com.sisdi.service.DepartmentServiceImp;
+import com.sisdi.model.Office;
 import com.sisdi.service.OfficeServiceImp;
-import com.sisdi.service.TempUserServiceImp;
-import com.sisdi.service.UserServiceImp;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SignatureException;
 import java.security.cert.CertificateException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import javax.naming.InvalidNameException;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -35,69 +36,85 @@ import signature.PDFSignatureInfoParser;
 /*
     El controlador de la pagina principal de SISDI
  */
-
 @Controller
 @Slf4j
 public class IndexController {
-    @Autowired 
-    private TempUserServiceImp tempUserService;
-    
-    @Autowired 
-    private DepartmentServiceImp departmentService;
-    
-    @Autowired 
-    private UserServiceImp userService;
-    
-    @Autowired 
-    private OfficeServiceImp officeService;
-    
-    @Autowired 
+
+    @Autowired
     private UserData userData;
-     
+    @Autowired
+    private OfficeServiceImp officeServiceImp;
+
     @GetMapping("/login")
-    public String login(){
-       return "login";
+    public String login() {
+        return "login";
     }
+
     @GetMapping("/")
     public String showIndex(Model model, @AuthenticationPrincipal User user, HttpSession session) {
+
         log.info("ejecutando el controlador Oficios");
         session.setAttribute("user", user);
         session.setAttribute("usuarioCompleto", userData.getUser(user.getUsername()));
         model.addAttribute("user", user);
-        return "index";
+        return "/index";
     }
+
     @GetMapping("/error")
     public String showError(Model model, @AuthenticationPrincipal User user, HttpSession session) {
-        log.info("ejecutando el controlador Oficios");
-        session.setAttribute("user", user);
-        session.setAttribute("usuarioCompleto", userData.getUser(user.getUsername()));
-        model.addAttribute("user", user);
+        if (!user.isAccountNonExpired()) {
+            return "/login";
+        }
         return "error";
     }
+
     @PostMapping("/singleFileUpload")
-    public ResponseEntity<?> singleFileUpload(Model model, @RequestParam("adjunto") MultipartFile file,HttpServletResponse response ) {
+    public ResponseEntity<?> singleFileUpload(Model model, @RequestParam("adjunto") MultipartFile file, HttpServletResponse response) {
         JSONObject obj = new JSONObject();
-        boolean signed=false;
+        boolean signed = false;
         if (file.isEmpty()) {
             return new ResponseEntity("please select a file!", HttpStatus.OK);
         }
         try {
             byte[] bytes = file.getBytes();
             List<PDFSignatureInfo> info = PDFSignatureInfoParser.getPDFSignatureInfo(bytes);
-            if(info.size()> 0){
-                signed=true;
-            }else{
-                signed=false;
-            }          
-        } catch (IOException | InvalidNameException | CertificateException| NoSuchAlgorithmException | InvalidKeyException |SignatureException | NoSuchProviderException e) {
+            if (info.size() > 0) {
+                signed = true;
+            } else {
+                signed = false;
+            }
+        } catch (IOException | InvalidNameException | CertificateException | NoSuchAlgorithmException | InvalidKeyException | SignatureException | NoSuchProviderException e) {
             model.addAttribute("message", "Cannot open file: " + e.getMessage());
             e.printStackTrace();
         }
-         obj.put("name", file.getOriginalFilename());
-         obj.put("size", file.getSize());
-         obj.put("resource", file.getResource());
-         obj.put("signed", signed);       
+        obj.put("name", file.getOriginalFilename());
+        obj.put("size", file.getSize());
+        obj.put("resource", file.getResource());
+        obj.put("signed", signed);
         return new ResponseEntity(obj.toString(), new HttpHeaders(), HttpStatus.OK);
     }
-     
+
+    @PostMapping("/notificationResponse")
+    public ResponseEntity<?> notificationResponse(Model model, @AuthenticationPrincipal User user, HttpSession session) {
+        List<Office> offices = officeServiceImp.listOfficeByReceptor(user.getUsername());
+        List<Office> officesDate = new ArrayList<Office>();
+        JSONArray officesF=new JSONArray();
+        for (Office o : offices) {
+            if (o.getDEADLINE() != null) {
+                if (o.getSTATE() != 2) {
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
+                    String strDate= formatter.format(o.getDEADLINE());  
+                    JSONObject obj = new JSONObject();
+                    obj.put("Offnumber", o.getOFFNUMBER());
+                    obj.put("Deadline", strDate);
+                    obj.put("Emisor", o.getUSER_ID());
+                    officesF.put(obj);
+                }
+            }
+        }
+        log.info(officesDate.toString());
+
+        return new ResponseEntity(officesF.toString(), new HttpHeaders(), HttpStatus.OK);
+    }
+
 }
